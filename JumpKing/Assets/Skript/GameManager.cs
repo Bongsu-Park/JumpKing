61dd2e88ec73b3670a725b084f42fd9b3990bb8b
Bongsu-Park <qkrqhdtn98@naver.com>
2022-06-26 19:38:12 +0900

    Git에 기존의 모작 업로드

---
+++ JumpKing/Assets/Skript/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	[SerializeField] GameObject player;
+	[SerializeField] Camera MainCamera;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		MainCamera.transform.position = new Vector3(0, ((int)player.transform.position.y / 10) * 10 + 5, -10);
+	}
+}
